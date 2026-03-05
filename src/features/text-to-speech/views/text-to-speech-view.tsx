5fada5990759aa505aa15b5bdd7f75f01abfc02b
MouleeswaranR <ffmouleesh@gmail.com>
2026-03-05 20:13:19 +0530

    Text-To-Speech UI Added

---
+++ src/features/text-to-speech/views/text-to-speech-view.tsx
@@ -0,0 +1,20 @@
+"use client";
+import { SettingsPanel } from "../components/settings-panel";
+import { TextInputPanel } from "../components/text-input-panel";
+import { VoicePreviewPlaceholder } from "../components/voice-review-placeholder";
+
+import { TextToSpeechForm,defaultTTSValues } from "../components/text-to-speech-from";
+
+export function TextToSpeechView(){
+    return(
+        <TextToSpeechForm defaultValues={defaultTTSValues}>
+        <div className="flex min-h-0 flex-1 overflow-hidden">
+            <div className="flex min-h-0 flex-1 flex-col">
+                <TextInputPanel/>
+                <VoicePreviewPlaceholder/>
+            </div>
+            <SettingsPanel/>
+        </div>
+        </TextToSpeechForm>
+    )
+}
